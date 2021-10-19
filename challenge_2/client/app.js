1ebeb2054c42d14262db2d26f96fbba86abe52be
Erik Dan Tran <erikdantran99@gmail.com>
2021-10-19 15:06:05 -0500

    Set up server and form submission

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,10 @@
+// will need the AJAX POST and GET
+console.log('hello from app.js')
 
-
+// const form = document.getElementById('form');
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault()
+//   console.log('form submitted');
+//   console.log(event)
+//   console.log(event.target.value)
+// })
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,33 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors')
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(express.static('client')) // ask if the sendfile is needed
+app.use(express.urlencoded({
+  extended:true
+}))
 
 
+app.post('/upload_json', (req, res) => {
+  console.log('Made it to the server, body:', req.body)
+  res.send('post successful')
+})
+
+// app.get('/', (req, res) => {
+//   res.sendFile('index.html', (err) => {
+//     if (err) { console.log(err)}
+//     else { console.log('File sent')}
+//   })
+// })
+
+
+app.listen(port, () => {
+  console.log(`listening on localport:${port}`);
+})
